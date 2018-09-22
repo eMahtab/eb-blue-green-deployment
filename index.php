b034237960d0fdc887e0038ba5a6437e967d1f1d
Mahtab Alam <alammahtab08@gmail.com>
2018-09-22 21:44:13 +0530

    Blue/Green deployment with Elastic Beanstalk

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+	background: #BE93C5;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #7BC6CC, #BE93C5);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #7BC6CC, #BE93C5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+	</style>
+</head>
+<body>
+    <h1 align="center">App v2 :)</h1>
+</body>
+</html>
